79abe3b2d6f0f1790dae0b20727bcf3c90b59c89
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-10-11 15:19:51 +0200

    RMLMapper: add RMLMapper executor

--- bench_executor/container.py
+++ bench_executor/container.py
@@ -5,24 +5,30 @@ from time import sleep
 WAIT_TIME = 1
 
 class Container():
-    def __init__(self, container: str, name: str, ports: dict):
+    def __init__(self, container: str, name: str, ports: dict = {},
+                 volumes: dict = {}):
         self._client = docker.from_env()
         self._container = None
         self._container_name = container
         self._name = name
         self._ports = ports
+        self._volumes = volumes
 
-    def run(self, command: str = ''):
+    def run(self, command: str = '', detach=True):
         self._container = self._client.containers.run(self._container_name,
                                                       command, name=self._name,
-                                                      detach=True, remove=True,
-                                                      ports=self._ports)
+                                                      detach=detach, remove=True,
+                                                      ports=self._ports,
+                                                      volumes=self._volumes)
+
+    def logs(self):
+        return self._container.logs(stream=True)
 
     def run_and_wait_for_log(self, log_line: str, command: str =''):
         print(f'Starting "{self._name}" ({self._container_name}): "{command}"')
         self.run(command)
 
-        for line in self._container.logs(stream=True):
+        for line in self.logs():
             line = str(line.strip())
             if log_line in line:
                 sleep(WAIT_TIME)
--- bench_executor/rmlmapper.py
+++ bench_executor/rmlmapper.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
-from docker import Docker
+from container import Container
 
-class RMLMapper(Docker):
-    def __init__(self):
-        super().__init__()
+VERSION = '6.0.0'
 
-    def execute(self):
-        pass
+class RMLMapper(Container):
+    def __init__(self, data_path):
+        super().__init__(f'kg-construct/rmlmapper:v{VERSION}', 'RMLMapper',
+                         volumes=[f'{data_path}:/data'])
+
+    def execute(self, arguments):
+        # Example: [ '-m' 'mapping.rml.ttl' ]
+        self.run(f'java -jar rmlmapper/rmlmapper.jar {" ".join(arguments)}')
+        for line in self.logs():
+            print(str(line.strip()))
