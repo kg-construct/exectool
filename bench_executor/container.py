ce91114c743f2e455714626ce39639be46330328
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-10-11 15:42:32 +0200

    PostgreSQL: add PostgreSQL executor

--- bench_executor/container.py
+++ bench_executor/container.py
@@ -6,19 +6,21 @@ WAIT_TIME = 1
 
 class Container():
     def __init__(self, container: str, name: str, ports: dict = {},
-                 volumes: dict = {}):
+                 environment: dict = {}, volumes: dict = {}):
         self._client = docker.from_env()
         self._container = None
         self._container_name = container
         self._name = name
         self._ports = ports
         self._volumes = volumes
+        self._environment = environment
 
     def run(self, command: str = '', detach=True):
         self._container = self._client.containers.run(self._container_name,
                                                       command, name=self._name,
                                                       detach=detach, remove=True,
                                                       ports=self._ports,
+                                                      environment=self._environment,
                                                       volumes=self._volumes)
 
     def logs(self):
@@ -30,6 +32,7 @@ class Container():
 
         for line in self.logs():
             line = str(line.strip())
+            print(line)
             if log_line in line:
                 sleep(WAIT_TIME)
                 print(f'Container "{self._name}" running!')
--- bench_executor/postgresql.py
+++ bench_executor/postgresql.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
-from docker import Docker
+from container import Container
 
-class PostrgreSQL(Docker):
-    def __init__(self):
-        super().__init__()
+class PostgreSQL(Container):
+    def __init__(self, data_path: str):
+        super().__init__('postgres:14.5-alpine', 'PostgreSQL',
+                         ports={'5432': '5432'},
+                         environment={'POSTGRES_PASSWORD': 'root',
+                                      'POSTGRES_USER': 'root',
+                                      'POSTGRES_DB': 'db',
+                                      'POSTGRES_HOST_AUTH_METHOD': 'trust'},
+                         volumes=[f'{data_path}:/var/lib/postgresql/data'])
 
     def load(self):
         pass
