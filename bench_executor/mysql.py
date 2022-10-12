c2a8a66532fec70343e8c33a1835a32a47643b8e
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-10-12 09:34:37 +0200

    MySQL: add MySQL executor

--- bench_executor/mysql.py
+++ bench_executor/mysql.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
-from docker import Docker
+from container import Container
 
-class MySQL(Docker):
-    def __init__(self):
-        super().__init__()
+VERSION = '8.0'
+
+class MySQL(Container):
+    def __init__(self, data_path: str):
+        super().__init__(f'mysql:{VERSION}-debian', 'MySQL',
+                         ports={'3306':'3306'},
+                         environment={'MYSQL_ROOT_PASSWORD': 'root',
+                                      'MYSQL_DATABASE': 'db'},
+                         volumes=[f'{data_path}/mysql/data:/var/lib/mysql',
+                                  f'{data_path}/mysql/mysql-secure-file-prive.cnf:'
+                                  '/etc/mysql/conf.d/mysql-secure-file-prive.cnf'])
+
+    def wait_until_ready(self, command=''):
+        self.run_and_wait_for_log('port: 3306  MySQL Community Server - GPL.', command=command)
 
     def load(self):
         pass
