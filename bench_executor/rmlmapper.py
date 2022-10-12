055969b809acee7258f0c87a0ba78a4cc0228773
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-10-12 09:35:11 +0200

    RMLMapper: specify volume name in class

--- bench_executor/rmlmapper.py
+++ bench_executor/rmlmapper.py
@@ -7,7 +7,7 @@ VERSION = '6.0.0'
 class RMLMapper(Container):
     def __init__(self, data_path):
         super().__init__(f'kg-construct/rmlmapper:v{VERSION}', 'RMLMapper',
-                         volumes=[f'{data_path}:/data'])
+                         volumes=[f'{data_path}/rmlmapper:/data'])
 
     def execute(self, arguments):
         # Example: [ '-m' 'mapping.rml.ttl' ]
