3588ef8be8a834ba4612ae5900270955bb6ace27
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-10-13 14:38:15 +0200

    Query: add query executor

--- bench_executor/query.py
+++ bench_executor/query.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+
+import requests
+
+class Query():
+    def __init__(self, query: str):
+        self._query = query
+
+    def execute(self, sparql_endpoint: str) -> str:
+        data = {
+            'query': self._query,
+            'format': 'text/plain', # N-Triples Virtuoso
+            'default-graph-uri': '' # Empty default graph Virtuoso
+        }
+        r = requests.post(sparql_endpoint, data=data)
+        r.raise_for_status()
+        return r.text
+
+    def execute_and_save(self, sparql_endpoint: str, results_file: str):
+        results = self.execute(sparql_endpoint)
+        with open(results_file, 'w') as f:
+            f.write(results)
--- bench_executor/virtuoso.py
+++ bench_executor/virtuoso.py
@@ -10,9 +10,14 @@ class Virtuoso(Container):
                          ports={'8890':'8890', '1111':'1111'},
                          environment={'DBA_PASSWORD':'root'},
                          volumes=[f'{data_path}/virtuoso:/database'])
+        self._endpoint = 'http://localhost:8890/sparql'
 
     def wait_until_ready(self, command=''):
         self.run_and_wait_for_log('Server online at', command=command)
 
     def load(self):
         pass
+
+    @property
+    def endpoint(self):
+        return self._endpoint
