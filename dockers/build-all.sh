5a5f99bf1a9728c361a55677300a735e88a31ab7
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-10-25 14:35:25 +0200

    dockers: fix build script

--- dockers/build-all.sh
+++ dockers/build-all.sh
@@ -9,7 +9,7 @@ cd ..
 # Morph-KGC 2.2.0
 echo "Build Morph-KGC 2.2.0 ..."
 cd Morph-KGC
-docker build -t kg-construct/morph-kgc:v2.2.0
+docker build -t kg-construct/morph-kgc:v2.2.0 .
 cd ..
 
 # Morph-RDB 3.12.5
@@ -20,17 +20,17 @@ docker build -t kg-construct/morph-rdb:v3.12.5 .
 # Ontop 4.2.0-PATCH
 echo "Building Ontop 4.2.0-PATCH ..."
 cd Ontop
-docker build -f "Dockerfile.source" -t kg-construct/ontop:v4.2.1-PATCH
+docker build -f "Dockerfile.source" -t kg-construct/ontop:v4.2.1-PATCH .
 cd ..
 
 # RMLMapper 6.0.0
 echo "Building RMLMapper 6.0.0 ..."
 cd RMLMapper
-docker build -t kg-construct/rmlmapper:v6.0.0
+docker build -t kg-construct/rmlmapper:v6.0.0 .
 cd ..
 
 # SDM-RDFizer 4.6
 echo "Building SDM-RDFizer 4.6 ..."
 cd SDM-RDFizer
-docker build -t kg-construct/sdmrdfizer:v4.6
+docker build -t kg-construct/sdmrdfizer:v4.6 .
 cd ..
