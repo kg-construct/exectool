aa4c59b315337a614dd5dc471b4aeea7deb6bcab
Dylan Van Assche <dylan.vanassche@ugent.be>
2022-11-15 19:50:57 +0100

    YARRRML: add YARRRML support

---
+++ bench_executor/yarrrml.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import psutil
+from container import Container
+
+VERSION = '1.3.6'
+
+class YARRRML(Container):
+    def __init__(self, data_path: str, config_path: str, verbose: bool):
+        self._verbose = verbose
+        self._data_path = os.path.abspath(data_path)
+        self._config_path = os.path.abspath(config_path)
+        os.makedirs(os.path.join(self._data_path, 'yarrrml'), exist_ok=True)
+        super().__init__(f'dylanvanassche/yarrrml:v{VERSION}', 'YARRRML',
+                         volumes=[f'{self._data_path}/yarrrml:/data',
+                                  f'{self._data_path}/shared:/data/shared'])
+
+    @property
+    def root_mount_directory(self) -> str:
+        return __name__.lower()
+
+    def transform_mapping(self, yarrrml_file: str, mapping_file: str,
+                          r2rml: bool = False, pretty: bool = True):
+        arguments = ['-i', os.path.join('/data/shared/', yarrrml_file),
+                     '-o', os.path.join('/data/shared/', mapping_file)]
+
+        if r2rml:
+            arguments.append('-c')
+            arguments.append('-f R2RML')
+
+        if pretty:
+            arguments.append('-p')
+
+        self._logs.append(f'Executing YARRRML with arguments '
+                          f'"{" ".join(arguments)}"\n')
+
+        cmd = f'{" ".join(arguments)}'
+        success = self.run_and_wait_for_exit(cmd)
+        return success
--- dockers/build-all.sh
+++ dockers/build-all.sh
@@ -19,70 +19,70 @@ RMLMAPPER_BUILD='363'
 SDMRDFIZER_VERSION='4.6.3'
 YARRRML_VERSION='1.3.6'
 
-## MySQL
-#echo "*** Building MySQL $MYSQL_VERSION ... ***"
-#cd MySQL
-#docker build --build-arg MYSQL_VERSION=$MYSQL_VERSION \
-#    -t dylanvanassche/mysql:v$MYSQL_VERSION .
-#docker push dylanvanassche/mysql:v$MYSQL_VERSION
-#cd ..
-#
-## PostgreSQL
-#echo "*** Building PostgreSQL $POSTGRESQL_VERSION ... ***"
-#cd PostgreSQL
-#docker build --build-arg POSTGRESQL_VERSION=$POSTGRESQL_VERSION \
-#    -t dylanvanassche/postgresql:v$POSTGRESQL_VERSION .
-#docker push dylanvanassche/postgresql:v$POSTGRESQL_VERSION
-#cd ..
-#
-## Virtuoso
-#echo "*** Building Virtuoso $VIRTUOSO_VERSION ... ***"
-#cd Virtuoso
-#docker build --build-arg VIRTUOSO_VERSION=$VIRTUOSO_VERSION \
-#    -t dylanvanassche/virtuoso:v$VIRTUOSO_VERSION .
-#docker push dylanvanassche/virtuoso:v$VIRTUOSO_VERSION
-#cd ..
-## Fuseki
-#echo "*** Building Fuseki $FUSEKI_VERSION ... ***"
-#cd Fuseki
-#docker build --build-arg JENA_VERSION=$FUSEKI_VERSION \
-#    -t dylanvanassche/fuseki:v$FUSEKI_VERSION .
-#docker push dylanvanassche/fuseki:v$FUSEKI_VERSION
-#cd ..
-#
-## Morph-KGC
-#echo "*** Building Morph-KGC $MORPHKGC_VERSION ... ***"
-#cd Morph-KGC
-#docker build --build-arg MORPHKGC_VERSION=$MORPHKGC_VERSION \
-#    -t dylanvanassche/morph-kgc:v$MORPHKGC_VERSION .
-#docker push dylanvanassche/morph-kgc:v$MORPHKGC_VERSION
-#cd ..
-#
-## Morph-RDB
-#echo "*** Building Morph-RDB $MORPHRDB_VERSION ... ***"
-#cd Morph-RDB
-#docker build --build-arg MORPHRDB_VERSION=$MORPHRDB_VERSION \
-#    -t dylanvanassche/morph-rdb:v$MORPHRDB_VERSION .
-#docker push dylanvanassche/morph-rdb:v$MORPHRDB_VERSION
-#cd ..
-#
-## Ontop
-#echo "*** Building Ontop $ONTOP_VERSION ... ***"
-#cd Ontop
-#docker build --no-cache -f "Dockerfile.source" \
-#    --build-arg ONTOP_VERSION=$ONTOP_VERSION \
-#    -t dylanvanassche/ontop:v$ONTOP_VERSION .
-#docker push dylanvanassche/ontop:v$ONTOP_VERSION
-#cd ..
-#
-## RMLMapper
-#echo "*** Building RMLMapper $RMLMAPPER_VERSION r$RMLMAPPER_BUILD ... ***"
-#cd RMLMapper
-#docker build --build-arg RMLMAPPER_VERSION=$RMLMAPPER_VERSION \
-#    --build-arg RMLMAPPER_BUILD=$RMLMAPPER_BUILD \
-#    -t dylanvanassche/rmlmapper:v$RMLMAPPER_VERSION .
-#docker push dylanvanassche/rmlmapper:v$RMLMAPPER_VERSION
-#cd ..
+# MySQL
+echo "*** Building MySQL $MYSQL_VERSION ... ***"
+cd MySQL
+docker build --build-arg MYSQL_VERSION=$MYSQL_VERSION \
+    -t dylanvanassche/mysql:v$MYSQL_VERSION .
+docker push dylanvanassche/mysql:v$MYSQL_VERSION
+cd ..
+
+# PostgreSQL
+echo "*** Building PostgreSQL $POSTGRESQL_VERSION ... ***"
+cd PostgreSQL
+docker build --build-arg POSTGRESQL_VERSION=$POSTGRESQL_VERSION \
+    -t dylanvanassche/postgresql:v$POSTGRESQL_VERSION .
+docker push dylanvanassche/postgresql:v$POSTGRESQL_VERSION
+cd ..
+
+# Virtuoso
+echo "*** Building Virtuoso $VIRTUOSO_VERSION ... ***"
+cd Virtuoso
+docker build --build-arg VIRTUOSO_VERSION=$VIRTUOSO_VERSION \
+    -t dylanvanassche/virtuoso:v$VIRTUOSO_VERSION .
+docker push dylanvanassche/virtuoso:v$VIRTUOSO_VERSION
+cd ..
+# Fuseki
+echo "*** Building Fuseki $FUSEKI_VERSION ... ***"
+cd Fuseki
+docker build --build-arg JENA_VERSION=$FUSEKI_VERSION \
+    -t dylanvanassche/fuseki:v$FUSEKI_VERSION .
+docker push dylanvanassche/fuseki:v$FUSEKI_VERSION
+cd ..
+
+# Morph-KGC
+echo "*** Building Morph-KGC $MORPHKGC_VERSION ... ***"
+cd Morph-KGC
+docker build --build-arg MORPHKGC_VERSION=$MORPHKGC_VERSION \
+    -t dylanvanassche/morph-kgc:v$MORPHKGC_VERSION .
+docker push dylanvanassche/morph-kgc:v$MORPHKGC_VERSION
+cd ..
+
+# Morph-RDB
+echo "*** Building Morph-RDB $MORPHRDB_VERSION ... ***"
+cd Morph-RDB
+docker build --build-arg MORPHRDB_VERSION=$MORPHRDB_VERSION \
+    -t dylanvanassche/morph-rdb:v$MORPHRDB_VERSION .
+docker push dylanvanassche/morph-rdb:v$MORPHRDB_VERSION
+cd ..
+
+# Ontop
+echo "*** Building Ontop $ONTOP_VERSION ... ***"
+cd Ontop
+docker build --no-cache -f "Dockerfile.source" \
+    --build-arg ONTOP_VERSION=$ONTOP_VERSION \
+    -t dylanvanassche/ontop:v$ONTOP_VERSION .
+docker push dylanvanassche/ontop:v$ONTOP_VERSION
+cd ..
+
+# RMLMapper
+echo "*** Building RMLMapper $RMLMAPPER_VERSION r$RMLMAPPER_BUILD ... ***"
+cd RMLMapper
+docker build --build-arg RMLMAPPER_VERSION=$RMLMAPPER_VERSION \
+    --build-arg RMLMAPPER_BUILD=$RMLMAPPER_BUILD \
+    -t dylanvanassche/rmlmapper:v$RMLMAPPER_VERSION .
+docker push dylanvanassche/rmlmapper:v$RMLMAPPER_VERSION
+cd ..
 
 # SDM-RDFizer
 echo "*** Building SDM-RDFizer $SDMRDFIZER_VERSION ... ***"
